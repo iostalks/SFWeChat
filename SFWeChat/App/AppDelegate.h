789a8ec07bae3cdf9e98521d604f3b05db44013d
developer <developer@idelan.cn>
2016-11-11 10:34:55 +0800

    first commit

--- SFWeChat/App/AppDelegate.h
+++ SFWeChat/App/AppDelegate.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  AppDelegate.h
 //  SFWeChat
 //
 //  Created by Jone on 11/11/2016.
@@ -8,8 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
 
+@property (strong, nonatomic) UIWindow *window;
 
 @end
 
