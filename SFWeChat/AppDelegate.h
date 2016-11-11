4b173d9269ab1d327076ff423896742f18392547
developer <developer@idelan.cn>
2016-11-11 09:50:21 +0800

    Initial Commit

---
+++ SFWeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SFWeChat
+//
+//  Created by Jone on 11/11/2016.
+//  Copyright © 2016 Jone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
